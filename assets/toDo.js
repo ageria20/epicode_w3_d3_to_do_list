a4b745ece8c02c04c7613d1bda97874b29d5519d
Andrea <ageria20@gmail.com>
2024-05-15 17:00:04 +0200

    aggiunta commenti

--- assets/toDo.js
+++ assets/toDo.js
@@ -5,16 +5,23 @@ const activityForm = document.getElementById("activity");
 
 addBtn.addEventListener("click", function (event) {
   event.preventDefault(); // Impedisce il refresh della pagina
+  // prendo il valore di testo inserito nel campo input
   const inputText = document.getElementById("input-Text").value;
-  const newActivity = document.createElement("li");
-  newActivity.innerText = inputText;
   const newCont = document.createElement("ol");
+  // Creo un elemento lista per ogni attivita' che vado ad aggiungere
+  const newActivity = document.createElement("li");
+  // prendo il testo inserito nel campo input e lo aggiungo all'elemento lista
+  newActivity.innerText = inputText;
+  // creo uno stile all'elemento lista
   newActivity.style.display = "inline-block";
+  // creo un elemento input di tipo checkbox
   const checkBox = document.createElement("input");
   checkBox.type = "checkbox";
+  // aggiungo tutto al contenitore
   newCont.append(newActivity, checkBox);
+  // aggiungo la nuova attivita' e il checkbox al form
   activityForm.appendChild(newCont);
-  if (checkBox.checked === true) newActivity.classList.add("overline-text");
+  if (checkBox.checkValidity()) newActivity.classList.add("overline-text");
   else newActivity.classList.remove("overline-text");
   document.getElementById("input-Text").value = "";
   // Modificato da activityForm a activityList per appendere i checkbox al div e non al form
@@ -23,7 +30,8 @@ addBtn.addEventListener("click", function (event) {
 delBtn.addEventListener("click", function (event) {
   event.preventDefault(); // Impedisce il refresh della pagina
   //   newActivity.type = "checkbox";
-
+  // creo una variobile per prendere l'ultima attivita' aggiunta
   const lastChild = activityForm.lastChild;
+  // e la cancello
   activityForm.removeChild(lastChild);
 });
