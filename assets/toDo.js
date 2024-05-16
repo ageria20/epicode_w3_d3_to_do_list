143e2c647a5b829457f62cdfb11773bde0503360
Andrea <ageria20@gmail.com>
2024-05-16 22:15:52 +0200

    aggiunta toggle overLine text

--- assets/toDo.js
+++ assets/toDo.js
@@ -3,27 +3,36 @@ const addBtn = document.getElementById("add-btn"); // Corretto da "add-bnt" a "a
 const delBtn = document.getElementById("del-btn"); // Corretto da "del-bnt" a "del-btn"
 const activityForm = document.getElementById("activity");
 
+// TODO: aggiungere la funzionlita' testo sbarrato
+
 addBtn.addEventListener("click", function (event) {
-  event.preventDefault(); // Impedisce il refresh della pagina
+  // Impedisce il refresh della pagina
+  event.preventDefault();
+
   // prendo il valore di testo inserito nel campo input
   const inputText = document.getElementById("input-Text").value;
-  const newCont = document.createElement("ol");
+
+  // questo non serve //const newCont = document.createElement("ol");
   // Creo un elemento lista per ogni attivita' che vado ad aggiungere
-  const newActivity = document.createElement("li");
+  const newActivity = document.createElement("p");
   // prendo il testo inserito nel campo input e lo aggiungo all'elemento lista
   newActivity.innerText = inputText;
   // creo uno stile all'elemento lista
   newActivity.style.display = "inline-block";
+  newActivity.addEventListener("click", function () {
+    newActivity.classList.toggle("overline-text");
+  });
   // creo un elemento input di tipo checkbox
-  const checkBox = document.createElement("input");
-  checkBox.type = "checkbox";
-  // aggiungo tutto al contenitore
-  newCont.append(newActivity, checkBox);
+  if (inputText === "") alert("campo vuoto");
+  else {
+    const checkBox = document.createElement("input");
+    checkBox.type = "checkbox";
+    // aggiungo tutto al contenitore
+    activityForm.append(newActivity, checkBox);
+    document.getElementById("input-Text").value = "";
+  }
   // aggiungo la nuova attivita' e il checkbox al form
-  activityForm.appendChild(newCont);
-  if (checkBox.checkValidity()) newActivity.classList.add("overline-text");
-  else newActivity.classList.remove("overline-text");
-  document.getElementById("input-Text").value = "";
+
   // Modificato da activityForm a activityList per appendere i checkbox al div e non al form
 });
 
